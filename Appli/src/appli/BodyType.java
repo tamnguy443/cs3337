d5206641a05a4f31e2bda2362d8f49e41064227d
Anthony Viramontes <47967342+moses1252@users.noreply.github.com>
2020-02-19 20:38:43 -0800

    Add files via upload

---
+++ Appli/src/appli/BodyType.java
@@ -0,0 +1,22 @@
+package profile;
+
+public class BodyType {
+	//bulk
+	//shredding
+	//maintain
+	//data field of routein is different based on costructors output
+	public BodyType(String bType) {
+		if (bType.equals("bulk")) {
+			
+		} else if ( bType.equals("shred")) {
+			
+		} else if (bType.equals("maintain")) {
+			
+		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Appli/src/appli/profile.java
@@ -0,0 +1,79 @@
+package profile;
+
+public class profile extends BodyType{
+
+	private String name;
+	private int weightPounds;
+	private int heightFeet;
+	private int heightInches;
+	private double bMI;
+	private BodyType bodyType;
+	
+	public profile(String bType, String name, int weightPounds, int heightInches) {
+		super(bType);
+		/*
+		 * 
+		 */
+		this.setBMI();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getWeightPounds() {
+		return weightPounds;
+	}
+
+	public void setWeightPounds(int weightPounds) {
+		this.weightPounds = weightPounds;
+	}
+
+	public int getHeightFeet() {
+		return heightFeet;
+	}
+
+	public void setHeightFeet(int heightFeet) {
+		this.heightFeet = heightFeet;
+	}
+
+	public int getHeightInches() {
+		return heightInches;
+	}
+
+	public void setHeightInches(int heightInches) {
+		this.heightInches = heightInches;
+	}
+
+	public BodyType getBodyType() {
+		return bodyType;
+	}
+
+	public void setBodyType(BodyType bodyType) {
+		this.bodyType = bodyType;
+	}
+	/*
+	 * age
+	 *bmi-The formula is BMI = kg/m2 where kg is a person's weight in kilograms 
+	 *and m2 is their height in metres squared.
+	 *
+	 *
+	 * 
+	 */
+	private void setBMI() {
+		this.bMI = (0.453592/this.weightPounds) / Math.pow(((0.3048/this.heightFeet) + (0.0254/this.heightInches)),2);
+		
+	}
+	
+		
+	
+	
+	
+	
+	
+
+}
