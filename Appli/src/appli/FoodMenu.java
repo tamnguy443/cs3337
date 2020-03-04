dc1898bb57519ba1684be195a7e6913b6ab6caba
tamnguy443 <60633642+tamnguy443@users.noreply.github.com>
2020-03-04 09:16:07 -0800

    Add files via upload

---
+++ Appli/src/appli/FoodMenu.java
@@ -0,0 +1,23 @@
+package appli;
+
+public class FoodMenu {
+	String nameOfFood;
+	int carbs;
+	int protein; //every gram of protein and crabs is 4 calories
+	int fat;
+	//add more int if needed for nutrients
+
+	public FoodMenu(String nameOfFood, int carbs, int protein, int fat) {
+		super();
+		this.nameOfFood = nameOfFood;
+		
+	}
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+
+
+}
