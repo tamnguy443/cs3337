2b1524e46b5ee660e8c4441c4fff025863bcaca5
tamnguy443 <60633642+tamnguy443@users.noreply.github.com>
2020-03-09 09:16:52 -0700

    Add files via upload

---
+++ Appli/src/appli/Schedule.java
@@ -0,0 +1,16 @@
+package appli;
+
+public class Schedule {
+	String sched;
+	
+	
+	public Schedule(String sched) {
+		this.sched = sched;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
