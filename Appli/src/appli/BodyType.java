b4f01ed215861dc4e871974b22fcbdda2af6b756
tamnguy443 <60633642+tamnguy443@users.noreply.github.com>
2020-03-15 18:06:19 -0700

    Update BodyType.java

--- Appli/src/appli/BodyType.java
+++ Appli/src/appli/BodyType.java
@@ -1,44 +1,58 @@
-package profile;
+package appli;
+
 
 public class BodyType {
-	private char bType;
+	private String bType;
 
 	// bulk
 	// shredding
 	// maintain
 	// data field of routein is different based on costructors output
 
-	public BodyType(String bType) {
+	protected BodyType(String bType) {
 
 		if (bType.equals("bulk")) {
-			this.bType = 'b';
+			this.bType = "b";
 		} else if (bType.equals("shred")) {
-			this.bType = 's';
+			this.bType = "s";
 		} else if (bType.equals("maintain")) {
-			this.bType = 'm';
-		}
+			this.bType = "m";
+		} 
 
+	}
+	
+	protected BodyType() {
+		//default constructor
+		this.bType = "";
+	}
+
+	protected String getbType() {
+		return bType;
+	}
+
+	protected void setbType(String bType) {
+		this.bType = bType;
 	}
 
 	// one method to call the private ones based on bodyType
 	protected void showMenu() {
 		// will call bulk/shred/maintain Menu method based on char bType
-		if (this.bType == 'b') {
+		if (this.bType.equals("b")) {
 			this.bulkMenu();
-		} else if (this.bType == 's') {
+		} else if (this.bType.equals("s")) {
 			this.shredMenu();
-		} else if (this.bType == 'm') {
+		} else if (this.bType.equals("m")) {
 			this.maintainMenu();
 		}
 	}
 	
 	protected void scheduler() {
 		// will call bulk/shred/maintain Menu method based on char bType
-		if (this.bType == 'b') {
+		if (this.bType.equals("b")) {
 			this.bulkSched();
-		} else if (this.bType == 's') {
+		} else if (this.bType.equals("s")) {
 			this.shredSched();
-		} else if (this.bType == 'm') {
+		} else if (this.bType.equals("m")) {
 			this.maintainSched();
 		}
 		
@@ -77,4 +91,3 @@ public class BodyType {
 	}
 
 }
-
