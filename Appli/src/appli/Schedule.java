f5d5206169693c5badb1d343ef4d63049d200031
tamnguy443 <60633642+tamnguy443@users.noreply.github.com>
2020-03-27 20:49:29 -0700

    Update Schedule.java

--- Appli/src/appli/Schedule.java
+++ Appli/src/appli/Schedule.java
@@ -1,11 +1,45 @@
 package appli;
 
 public class Schedule {
-	String sched;
-	
-	
+	private String sched;
+	private boolean dayOff = false;
+	private FoodMenu menu;
+
 	public Schedule(String sched) {
 		this.sched = sched;
+	}
+
+	/*
+	 * Monday to Saturday, Sunday off. 3 meals per day
+	 */
+	protected FoodMenu breakfast() {
+		if (!this.dayOff) {
+			return menu;
+		} else {
+			return menu;
+		}
+	}
+
+	protected FoodMenu lunch() {
+		if (!this.dayOff) {
+			return menu;
+		} else {
+			return menu;
+		}
+		
+	}
+
+	protected FoodMenu dinner() {
+		if (!this.dayOff) {
+			return menu;
+		} else {
+			return menu;
+		}
+		
+	}
+
+	protected void dayOff() {
+		dayOff = true;
 	}
 
 	public static void main(String[] args) {
@@ -14,3 +48,4 @@ public class Schedule {
 	}
 
 }
+
