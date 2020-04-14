60375815eb6ca972fb85838adf0c6b0af8d83690
tamnguy443 <60633642+tamnguy443@users.noreply.github.com>
2020-04-13 21:42:25 -0700

    Update Schedule.java

--- Appli/src/appli/Schedule.java
+++ Appli/src/appli/Schedule.java
@@ -1,51 +1,67 @@
 package appli;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Schedule {
 	private String sched;
 	private boolean dayOff = false;
-	private FoodMenu menu;
+	protected ArrayList<FoodMenu> userSelectedFood;
+	protected List<List<FoodMenu>> days;
 
-	public Schedule(String sched) {
+	public Schedule(String sched, ArrayList<FoodMenu> userSelectedFood) {
 		this.sched = sched;
+		this.userSelectedFood = userSelectedFood;
+		days = new ArrayList<>();
+	}
+
+	public Schedule(String a) {
+		
 	}
 
 	/*
-	 * Monday to Saturday, Sunday off. 3 meals per day
+	 * Monday to Saturday, Sunday off. 3 meals per day snack1 and snack2 breakfast
+	 * 8am - 8:30pm lunch: 12:30, snacksOne 3:00pm, snackTwo 5:30
 	 */
 	protected FoodMenu breakfast() {
 		if (!this.dayOff) {
-			return menu;
+			return days.get(0).get(0);
 		} else {
-			return menu;
+			return days.get(0).get(0);
 		}
 	}
 
 	protected FoodMenu lunch() {
 		if (!this.dayOff) {
-			return menu;
+			return days.get(0).get(0);
 		} else {
-			return menu;
+			return days.get(0).get(0);
 		}
-		
 	}
 
 	protected FoodMenu dinner() {
 		if (!this.dayOff) {
-			return menu;
+			return days.get(0).get(0);
 		} else {
-			return menu;
+			return days.get(0).get(0);
 		}
-		
+
 	}
 
 	protected void dayOff() {
 		dayOff = true;
 	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	protected void rotateMeal() {
+		FoodMenu last = this.userSelectedFood.get(this.userSelectedFood.size() - 1);
+		
+		for (int i = this.userSelectedFood.size() - 1; i > 0 ; i--) {
+			this.userSelectedFood.set(i, this.userSelectedFood.get(i - 1));
+		}
+		this.userSelectedFood.set(0, last);
 	}
 
-}
+	public void main(String[] args) {
 
+	}
+}
