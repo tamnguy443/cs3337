fc4cc95e00dacc6cb85336858122a8bda89558ae
tamnguy443 <60633642+tamnguy443@users.noreply.github.com>
2020-04-18 17:02:41 -0700

    Update Profile.java

--- Appli/src/appli/Profile.java
+++ Appli/src/appli/Profile.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Profile extends BodyType {
@@ -13,24 +14,41 @@ public class Profile extends BodyType {
 	private int weightPounds;
 	private int heightFeet;
 	private int heightInches;
+	private int age;
+	private String gender;
 	private double bMI;
 	private Schedule sched;
 	private BodyType bodyType;
-
-	private Profile(String bType, String name, String weightPounds, String heightInches, String heigthFeet, Schedule sched) {
-		super(bType);
-		this.name = name;
-		this.sched = sched;
-		this.setBMI(Integer.parseInt(weightPounds), Integer.parseInt(heightInches), Integer.parseInt(heigthFeet));
-	}
+	protected ArrayList<FoodMenu> selectedFood = new ArrayList<>(); //The foods the user selected
 	
 	public Profile() {
 		super();
 		this.readInfo();
+		this.getCaloriesIntake();
 	}
 
+	protected void getCaloriesIntake() {
+		bodyType.setCalcCaloriesIntake(this.gender, this.toKg(), this.toCm(), this.age,0);	
+	}
+	
 	public String getName() {
 		return name;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
 	}
 
 	public void setName(String name) {
@@ -69,14 +87,22 @@ public class Profile extends BodyType {
 		this.bodyType = bodyType;
 	}
 	
-	public Schedule getSched() {
-		return sched;
+	public String getSched() {
+		return "";
 	}
 
 	public void setSched(Schedule sched) {
 		this.sched = sched;
 	}
 
+	private double toKg() {
+		return this.weightPounds * 0.453592;
+	}
+	
+	private double toCm() {
+		return this.heightFeet * 30.48 + this.heightInches * 2.54;
+	}
+	
 	private void setBMI(int pounds, int feet, int inches) {
 		/*
 		 * age bmi-The formula is BMI = kg/m2 where kg is a person's weight in kilograms
@@ -99,12 +125,32 @@ public class Profile extends BodyType {
 		}
 		
 		this.name = in.nextLine();
+		this.age = Integer.parseInt(in.nextLine());
+		this.gender = in.nextLine();
 		this.weightPounds = Integer.parseInt(in.nextLine());
 		this.heightFeet = Integer.parseInt(in.nextLine());
 		this.heightInches = Integer.parseInt(in.nextLine());
 		this.sched = new Schedule(in.nextLine());
-		this.bodyType = new BodyType(in.nextLine());
-	
+		this.bodyType = new BodyType(in.nextLine(),this.sched.sched);
+		// next lines are food choices for selectedFood
+		while (in.hasNextLine()) {
+			String foodLine = in.nextLine();
+			boolean isIn = false;
+			for(int i = 0; i < this.selectedFood.size(); i++) {
+				if(this.selectedFood.get(i).getNameOfFood().equals(foodLine)) {
+					isIn = true;
+					break;
+				}
+			}
+			if(!isIn) {
+				FoodMenu ins = new FoodMenu();
+				for(int i = 0; i < ins.getMenuArr().size(); i++) {
+					if(ins.getMenuArr().get(i).getNameOfFood().equals(foodLine)) {
+						this.selectedFood.add(ins.getMenuArr().get(i));
+					}
+				}
+			}
+		}
 	}
 	
 	@Override
@@ -116,7 +162,7 @@ public class Profile extends BodyType {
 
 	protected static void writeFil() {
 		/*
-		 * write user info into a file after they finish taking first time quiz.
+		 * write user info into a file after they finish taking first time quiz
 		 */
 		File fil = new File("info.txt");
 		FileWriter fw = null;
